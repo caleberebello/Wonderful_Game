a43fe836ddd8360ca0a307c713f22eaf6f25ab2b
EnriqueIzel2 <eleaoizel@gmail.com>
2021-09-04 20:54:17 -0400

    First commit

---
+++ Assets/Scripts/MaskDude.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskDude : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private Animator anim;
+    public float speed;
+    public Transform rightCol;
+    public Transform leftCol;
+    public Transform headPoint;
+    private bool colliding;
+    public LayerMask layer;
+    public BoxCollider2D boxCollider2D;
+    public CircleCollider2D circleCollider2D;
+    public bool playerDestroyed;
+
+    
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();    
+    }
+
+    
+    void Update()
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position, layer);
+
+        if (colliding)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
+            speed *= -1f;
+        }
+    }
+
+     void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            float height = collision.contacts[0].point.y - headPoint.position.y;
+
+            if (height > 0 && !playerDestroyed)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                speed = 0;
+                anim.SetTrigger("die");
+                boxCollider2D.enabled = false;
+                circleCollider2D.enabled = false;
+                rig.bodyType = RigidbodyType2D.Kinematic;
+                Destroy(gameObject, .33f);
+            }
+            else
+            {
+                playerDestroyed = true;
+                GameController.instance.ShowGameOver();
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
